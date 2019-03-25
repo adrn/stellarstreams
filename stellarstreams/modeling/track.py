2c0706778ae7c95fab29ad8e13fc0dd340bdeb68
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-03-25 14:30:48 -0700

    adjust track determination stuff

--- stellarstreams/modeling/track.py
+++ stellarstreams/modeling/track.py
@@ -6,7 +6,8 @@ from scipy.stats import binned_statistic
 
 def get_stream_track(stream_c,
                      phi1_lim=[-180, 180]*u.deg,
-                     phi1_binsize=1*u.deg):
+                     phi1_binsize=1*u.deg,
+                     units=None):
     """TODO: document this shit
 
     Parameters
@@ -14,15 +15,28 @@ def get_stream_track(stream_c,
     stream_c : `~astropy.coordinates.BaseCoordinateFrame` subclass instance
     phi1_lim : `~astropy.units.Quantity` (optional)
     phi1_binsize : `~astropy.units.Quantity` (optional)
+    units : `dict`
 
     Returns
     -------
     mean_tracks : ``dict``
     std_tracks : ``dict``
     """
-    phi1 = stream_c.spherical.lon.wrap_at(180*u.deg).degree
-    phi1_lim = phi1_lim.to_value(u.deg)
-    phi1_binsize = phi1_binsize.to_value(u.deg)
+    # All position and velocity component names:
+    component_names = (
+        list(stream_c.get_representation_component_names().keys()) +
+        list(stream_c.get_representation_component_names('s').keys()))
+
+    # If no units are provided:
+    if units is None:
+        units = dict()
+
+    units['phi1'] = units.get('phi1',
+                              getattr(stream_c, component_names[0]).unit)
+
+    phi1 = stream_c.spherical.lon.wrap_at(180*u.deg).to_value(units['phi1'])
+    phi1_lim = phi1_lim.to_value(units['phi1'])
+    phi1_binsize = phi1_binsize.to_value(units['phi1'])
 
     phi1_bins = np.arange(phi1_lim[0], phi1_lim[1]+1e-8, phi1_binsize)
     phi1_binc = 0.5 * (phi1_bins[:-1] + phi1_bins[1:])
@@ -31,60 +45,25 @@ def get_stream_track(stream_c,
     stds = dict()
     mean_tracks = dict()
     std_tracks = dict()
-    units = dict()
 
-    k = 'phi2'
-    val = stream_c.spherical.lat
-    means[k] = binned_statistic(phi1, val.value,
-                                bins=phi1_bins, statistic='mean')
-    stds[k] = binned_statistic(phi1, val.value,
-                               bins=phi1_bins, statistic='std')
-    units[k] = val.unit
-    mean_tracks[k] = InterpolatedUnivariateSpline(phi1_binc,
-                                                  means[k].statistic,
-                                                  w=1/stds[k].statistic**2)
-    std_tracks[k] = InterpolatedUnivariateSpline(phi1_binc,
-                                                 stds[k].statistic)
-
-    k = 'distmod'
-    val = getattr(stream_c, 'distance').distmod
-    means[k] = binned_statistic(phi1, val.value,
-                                bins=phi1_bins, statistic='mean')
-    stds[k] = binned_statistic(phi1, val.value,
-                               bins=phi1_bins, statistic='std')
-    units[k] = val.unit
-    mean_tracks[k] = InterpolatedUnivariateSpline(phi1_binc, means[k].statistic,
-                                                  w=1/stds[k].statistic**2)
-    std_tracks[k] = InterpolatedUnivariateSpline(phi1_binc,
-                                                 stds[k].statistic)
-
-    has_vel = 's' in stream_c.data.differentials
-    if has_vel:
-        for k, k2 in [('pm1', 'd_lon_coslat'),
-                      ('pm2', 'd_lat')]:
-            val = getattr(stream_c.sphericalcoslat, k2)
-            means[k] = binned_statistic(phi1, val.value,
-                                        bins=phi1_bins, statistic='mean')
-            stds[k] = binned_statistic(phi1, val.value,
-                                       bins=phi1_bins, statistic='std')
+    for k in component_names[1:]:
+        val = getattr(stream_c, k)
+        if k in units:
+            val = val.to_value(units[k])
+        else:
             units[k] = val.unit
-            mean_tracks[k] = InterpolatedUnivariateSpline(
-                phi1_binc, means[k].statistic, w=1/stds[k].statistic**2)
+            val = val.value
 
-            std_tracks[k] = InterpolatedUnivariateSpline(phi1_binc,
-                                                         stds[k].statistic)
-
-        k = 'rv'
-        val = getattr(stream_c, 'radial_velocity')
-        means[k] = binned_statistic(phi1, val.value,
+        means[k] = binned_statistic(phi1, val,
                                     bins=phi1_bins, statistic='mean')
-        stds[k] = binned_statistic(phi1, val.value,
+        stds[k] = binned_statistic(phi1, val,
                                    bins=phi1_bins, statistic='std')
-        units[k] = val.unit
-        mean_tracks[k] = InterpolatedUnivariateSpline(phi1_binc,
-                                                      means[k].statistic,
-                                                      w=1/stds[k].statistic**2)
-        std_tracks[k] = InterpolatedUnivariateSpline(phi1_binc,
-                                                     stds[k].statistic)
+
+        mask = np.isfinite(means[k].statistic)
+        mean_tracks[k] = InterpolatedUnivariateSpline(phi1_binc[mask],
+                                                      means[k].statistic[mask])
+        mask = np.isfinite(stds[k].statistic)
+        std_tracks[k] = InterpolatedUnivariateSpline(phi1_binc[mask],
+                                                     stds[k].statistic[mask])
 
     return mean_tracks, std_tracks
